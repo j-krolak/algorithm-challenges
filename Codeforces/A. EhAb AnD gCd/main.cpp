37723a77f5f3fd364ab7aa43d9334186eb5761e8
Mars-- .- .- <jakubkrolak@protonmail.com>
2022-07-19 15:45:22 +0200

    Add A. EhAb AnD gCd

---
+++ Codeforces/A. EhAb AnD gCd/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		cout << x-1 <<" 1\n";
+	}
+	return 0;
+}
