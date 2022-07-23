b53b128b559e2836603b1bed7ceb6907e57ad9e8
PolishFighter <jakubkrolak@protonmail.com>
2022-07-23 21:29:11 +0200

    Add USACO 2015 December Contest, Silver Problem 2. High Card Wins

---
+++ USACO/USACO 2015 December Contest, Silver Problem 2. High Card Wins /main.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	freopen("highcard.in", "r", stdin);
+	freopen("highcard.out", "w", stdout);
+
+
+	int n;
+	cin >> n;
+
+	bool hasElsie[2*n+1] = {false};
+
+	for(int i = 0; i < n; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		hasElsie[tmp] = true;
+	}
+
+	vector<int> elsie, bessie;
+
+	for(int i = 1; i <= 2*n; i++)
+	{
+		if(hasElsie[i])
+		{
+			elsie.push_back(i);
+		}
+		else
+		{
+			bessie.push_back(i);
+		}
+
+	}
+
+	sort(elsie.begin(), elsie.end());
+	sort(bessie.begin(), bessie.end());
+
+	int points = 0;
+
+	int elsieI = 0;
+	int bessieI = 0;
+
+	while(elsieI <= elsie.size() && bessieI <= bessie.size())
+	{
+		if(bessie[bessieI] > elsie[elsieI])
+		{
+			points++;
+			elsieI++;
+			bessieI++;
+		}
+		else
+		{
+			bessieI++;
+		}
+	}
+
+	cout << points << "\n";
+
+	return 0;
+}
