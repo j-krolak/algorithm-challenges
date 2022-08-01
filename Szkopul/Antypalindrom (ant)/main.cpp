336c0b8730371e6f08bf2cb6029f2143c1b298a0
PolishFighter <jakubkrolak@protonmail.com>
2022-08-01 17:28:35 +0200

    Add OIJ Antypalindrom (ant)

---
+++ Szkopul/Antypalindrom (ant)/main.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin >> s;
+
+	long long result = s.size();
+	int w = -1;
+
+	for(int i = 1; i < s.size();i++)
+	{
+		if(s[i] != s[i-1])
+		{
+			if(i > 1 && s[i-2] != s[i])
+			{
+				result += i - w - 1;
+			}
+			else
+			{
+				result++;
+				w = i - 2;
+			}
+		}
+		else
+		{
+			w = i - 1;
+		}
+	}
+
+	cout << result << "\n";
+
+	return 0;
+}
